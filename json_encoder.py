838f92612423fdc80bffd12a98178f9c611cc1ba
Daniel Bielech <38240407+daCFniel@users.noreply.github.com>
2023-02-04 15:52:20 +0000

    Flash server running SIFT demo

---
+++ json_encoder.py
@@ -0,0 +1,10 @@
+from flask.json import JSONEncoder
+
+import numpy
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
--- test-data/make-request.py
+++ test-data/make-request.py
@@ -1,21 +1,29 @@
 import base64
 import json
 
+
 def encode_base64(file):
-	fh = open(file, "rb")
-	out_bytes = fh.read()
-	fh.close()
+    """
+    Encode to base64
+    :param file: file to encode
+    :return: encoded file
+    """
+    fh = open(file, "rb")
+    out_bytes = fh.read()
+    fh.close()
 
-	out_bytes_base64 = base64.b64encode(out_bytes)
-	out_bytes_base64 = out_bytes_base64.decode("ascii")
+    out_bytes_base64 = base64.b64encode(out_bytes)
+    out_bytes_base64 = out_bytes_base64.decode("ascii")
 
-	return out_bytes_base64
+    return out_bytes_base64
+
 
 piece = encode_base64("piece.png")
 base = encode_base64("base.jpg")
 
 req_dict = {
-	"imagedata": piece
+    "piece_data": piece,
+    "base_data": base
 }
 
 request = open("request.json", "wt")
