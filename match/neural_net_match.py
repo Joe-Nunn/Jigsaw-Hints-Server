90c880375e560ab9d2104a414f333241f20b688b
Joe-Nunn <Joe.Nunn@Hotmail.com>
2023-03-19 17:51:58 +0000

    Correct file path for model

--- match/neural_net_match.py
+++ match/neural_net_match.py
@@ -14,7 +14,7 @@ class NeuralNetMatch(Match):
         self.device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 
         self.model = NeuralNetwork()
-        self.model.load_state_dict(torch.load("model.pt", map_location=self.device))
+        self.model.load_state_dict(torch.load("match/model.pt", map_location=self.device))
         self.model.eval()
 
     @staticmethod
